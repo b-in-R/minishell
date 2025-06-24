465585c557aecd14a94f3d816929576c93fc154e
Alberto Outumuro Bueno <albertooutumurobueno@gmail.com>
2025-06-24 13:53:06 +0200

    Add files via upload
    signals update

--- srcs/signals/signal.c
+++ srcs/signals/signal.c
@@ -6,7 +6,7 @@
 /*   By: albertooutumurobueno <albertooutumurobu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 13:09:18 by albertooutu       #+#    #+#             */
-/*   Updated: 2025/06/13 16:06:39 by albertooutu      ###   ########.fr       */
+/*   Updated: 2025/06/24 11:36:13 by albertooutu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ volatile sig_atomic_t	g_signal = 0; // Variable globale pour stocker le status d
 void	sigint_handler(int sig)
 {
 	(void)sig; // On ignore la valeur de `sig`, mais il faut quand même l’argument pour respecter la signature du handler
-	g_signal = 1; // On signale qu'on a reçu un SIGINT (pour d'autres parties du programme, si besoin)
 	write(1, "\n", 1); // On saute une ligne proprement (comme Bash le fait)
 	rl_on_new_line(); // fonction builtin, readline se prépare à afficher un nouveau prompt
 	rl_replace_line("", 0); // readline efface la ligne en cours (l’utilisateur a peut-être tapé une commande partielle)
@@ -45,7 +44,7 @@ void	sigquit_handler(int sig)
 }
 
 /*Gère les signaux : Ctrl-C et Ctrl-\. Ctrl-D (via readline dans le main)
-* envoi le signal SIGINT (Ctrl-C) et appelle sigint_handler qui
+* envoi le signal SIGINT (Ctrl-C) et appelle sigint_handler
 * envoie le signal SIGQUIT/ Ctrl-\, et appelle sigquit_handler qui ignore le signal, comme dans bash/zsh
 */
 void	setup_signals(void)
