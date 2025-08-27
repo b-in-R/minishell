5e43f9170f2490f66eda5f80b2fc4959adf4dce0
Alberto Outumuro Bueno <aoutumur@c2r4s2.42lausanne.ch>
2025-08-27 18:48:43 +0200

    norminette syntax checker

--- srcs/parser/syntax_checker.c
+++ srcs/parser/syntax_checker.c
@@ -6,7 +6,7 @@
 /*   By: aoutumur <aoutumur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 15:51:19 by albertooutu       #+#    #+#             */
-/*   Updated: 2025/08/27 18:36:53 by aoutumur         ###   ########.fr       */
+/*   Updated: 2025/08/27 18:46:54 by aoutumur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
  *	Detecte sil y a une quote simple ou double non fermee
  *	Parcours la ligne caractere par caractere
  *	Quand une quote est ouverte on la stocke
- *	Si on retombe sur le meme type de quote apres, on la ferme
+ *	Si on retombe sur le meme type de quote apres,
+ *		ca veut dire quelle est fermee donc a la fin on retourne 0
  *	A la fin, si une quote est ouverte en retoune erreur
  */
 int	has_unclosed_quotes(const char *line)
@@ -67,9 +68,7 @@ int	check_syntax_errors(t_token *tokens, char *line)
 		if ((tokens->type == REDIR_IN || tokens->type == REDIR_OUT
 				|| tokens->type == REDIR_APPEND || tokens->type == HEREDOC)
 			&& (!tokens->next || tokens->next->type != WORD))
-		{
 			return (printf("Syntax error: redirection without target\n"), 1);
-		}
 		prev = tokens;
 		tokens = tokens->next;
 	}
