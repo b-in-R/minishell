9630e4626a17a4cda65a72e98c0ac36b26cd1cd2
Alberto Outumuro Bueno <albertooutumurobueno@gmail.com>
2025-09-10 12:26:32 +0200

    Add files via upload

--- srcs/builtin/builtin_1.c
+++ srcs/builtin/builtin_1.c
@@ -3,47 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_1.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rabiner <rabiner@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: albertooutumurobueno <albertooutumurobu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 17:30:26 by rabiner           #+#    #+#             */
-/*   Updated: 2025/09/02 19:28:59 by rabiner          ###   ########.fr       */
+/*   Updated: 2025/09/10 12:23:28 by albertooutu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-/*
-
-int	ft_echo(char **args)
-{
-	int	i;
-	int	j;
-	int	newline;
-
-	i = 1;
-	newline = 1;
-	while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
-	{
-		j = 1;
-		while (args[i][j++] == 'n')
-		if (args[i][j - 1] != '\0')
-			break ;
-		newline = 0;
-		i++;
-	}
-	while (args[i])
-	{
-		write(1, args[i], ft_strlen(args[i]));
-		if (args[i + 1])
-			write(1, " ", 1);
-		i++;
-	}
-	if (newline)
-		write(1, "\n", 1);
-	return (0);
-}
-*/
-
 
 int	ft_echo(char **args)
 {
@@ -94,12 +61,13 @@ int	ft_cd(char **my_env, char **args)
 
 int	ft_pwd(char **my_env, char **args)
 {
-	char	cwd[1024];
+	char cwd[1024];
 
 	(void)args;
 	if (getcwd(cwd, sizeof(cwd)))
 	{
-		printf("%s\n", cwd);
+		write(STDOUT_FILENO, cwd, ft_strlen(cwd));
+		write(STDOUT_FILENO, "\n", 1);
 		return (0);
 	}
 	error_exit(my_env, "ft_pwd");
